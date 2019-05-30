7a8ef57c32fb3377142add3a1d32f3bf9863a023
SaifRehman99 <saifr7493@gmail.com>
2019-05-30 15:33:21 +0500

    PUT added

--- Http Library (Ajax+Callbacks)/app.js
+++ Http Library (Ajax+Callbacks)/app.js
@@ -1,8 +1,40 @@
 const http = new library;
 
-// Get post
-http.get('https://jsonplaceholder.typicode.com/posts',(res)=>{
-    console.log(res);
+// GET Data
+http.get('https://jsonplaceholder.typicode. com/posts', (err, res) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(res);
+    }
 });
 
 console.log('posts');
+
+// Create Data
+const data = {
+
+    title: 'Saif Updated',
+    body: 'Saif Rehman',
+}
+// POST Data
+http.post('https://jsonplaceholder.typicode.com/posts', data, (err, res) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(res);
+    }
+})
+
+// PUT Data
+http.put('https://jsonplaceholder.typicode.com/posts/1',data,(err,res)=> {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(res);
+    }
+
+})
--- Http Library (Ajax+Callbacks)/library.js
+++ Http Library (Ajax+Callbacks)/library.js
@@ -11,7 +11,10 @@ library.prototype.get = function (url, callback) {
 
     this.http.onload = () => {
         if (this.http.status === 200) {
-            callback(this.http.responseText)
+            callback(null, this.http.responseText)
+        }
+        else {
+            callback(`Error : ${this.http.status}`)
         }
     }
 
@@ -21,5 +24,33 @@ library.prototype.get = function (url, callback) {
 
 
 // POST Request
+
+library.prototype.post = function (url, data, callback) {
+
+    this.http.open('POST', url, true);
+
+
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    this.http.onload = () => {
+        callback(null, this.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
 // PUT Request
+
+library.prototype.put = function (url, data, callback) {
+
+    this.http.open('PUT', url, true);
+
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    this.http.onload = () => {
+        callback(null, this.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
 // DELETE Request
