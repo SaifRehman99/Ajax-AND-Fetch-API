11a1bc084db84cf3749df7ce504204c5e52f512c
SaifRehman99 <saifr7493@gmail.com>
2019-05-30 19:09:18 +0500

    fetch with async added

---
+++ Http Library (Fetch + Async)/app.js
@@ -0,0 +1,36 @@
+const lib = new http;
+
+
+// GET HERE
+lib.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+
+
+
+// Creating Data here
+const data = {
+    name: 'Saif Rehman',
+    email: 'saifr9732@gmail.com',
+}
+
+
+// POST HERE
+lib.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+
+
+// PUT HERE
+lib.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+
+// DELETE HERE
+lib.delete('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+    
---
+++ Http Library (Fetch + Async)/library.js
@@ -0,0 +1,61 @@
+class http {
+
+    // GET HERE
+    async get(url) {
+
+        const resp = await fetch(url);
+
+        const data = await resp.json();
+
+        return data
+    }
+
+    // POST HERE
+    async post(url, data) {
+
+        const resp1 = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const data1 = await resp1.json();
+
+        return data1;
+    }
+
+
+
+    // PUT HERE
+   async put(url, data) {
+        const resp2 = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const data2 = await resp2.json();
+
+            return data2;
+          
+    }
+
+    // DELETE HERE
+    async delete(url) {
+        
+        const resp3 = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            });
+
+            const data3 = await 'Deleted..!';
+
+            return data3
+    }
+}
