99e90df213ac229a2c5d97828601b23149004c57
SaifRehman99 <saifr7493@gmail.com>
2019-05-30 18:08:52 +0500

    post fetch added

--- Http Library (Fetch + Promises)/app.js
+++ Http Library (Fetch + Promises)/app.js
@@ -1,6 +1,27 @@
 const lib = new http;
 
 
+// GET HERE
 lib.get('https://jsonplaceholder.typicode.com/users')
-.then(res => console.log(res))
-.catch(err => console.log(err))
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+
+
+
+// Creating Data here
+let data = {
+    name: 'Saif Rehman',
+    email: 'saifr9732@gmail.com',
+}
+
+
+// POST HERE
+lib.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+
+
+// PUT HERE
+// DELETE HERE
--- Http Library (Fetch + Promises)/library.js
+++ Http Library (Fetch + Promises)/library.js
@@ -1,4 +1,6 @@
 class http {
+
+    // GET HERE
     get(url) {
         return new Promise((resolve, reject) => {
             fetch(url)
@@ -8,4 +10,24 @@ class http {
 
         })
     }
+
+    // POST HERE
+    post(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+
+        })
+    }
+
+    // PUT HERE
+    // DELETE HERE
 }
